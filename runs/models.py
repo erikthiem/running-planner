c265a352c2c8080c2b0eee80732f5f61e1e6e55c
Erik Thiem <erikthiem@gmail.com>
2024-07-09 21:41:50 -0400

    Add user as foreign key to runs

--- runs/models.py
+++ runs/models.py
@@ -1,8 +1,11 @@
+from django.conf import settings
 from django.db import models
 
 class Run(models.Model):
     distance_miles = models.FloatField()
     start_time = models.DateTimeField()
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
     def __str__(self):
         return f'{self.distance_miles} miles started at {self.start_time}'
